7e8de305bfb1b7435d8a526c10a4fe49ee28d8a9
rcdbudget4a-cyber <rcdbudget4a@gmail.com>
2026-07-23 21:40:27 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.PRO4A_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwrsTlgrm-Gx_6k2gzDoX6GpVn0Uij7dWq41beXIl2o77detEHIGcXM7DbzQwlFk6zG9g/exec",
-  APP_NAME: "PRO 4A KIPO/WIPO Claims Monitoring System",
-  VERSION: "Phase 1.0"
-};
-
