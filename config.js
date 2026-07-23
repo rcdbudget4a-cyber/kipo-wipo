e9236daae9e3b79c69d77ffd7dc80bd4b9546b6d
rcdbudget4a-cyber <rcdbudget4a@gmail.com>
2026-07-23 21:07:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PRO4A_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzmhSkdqF_qsNB8BXm70yeoqwu0fXuJ8vC21yDt5-s2eRy3O_ubq8aeSbR7Zm9wlwrLTg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwrsTlgrm-Gx_6k2gzDoX6GpVn0Uij7dWq41beXIl2o77detEHIGcXM7DbzQwlFk6zG9g/exec",
   APP_NAME: "PRO 4A KIPO/WIPO Claims Monitoring System",
   VERSION: "Phase 1.0"
 };
